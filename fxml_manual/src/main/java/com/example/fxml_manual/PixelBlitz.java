b25f554f0a4f93a2bdd6f39115249f12996fcaa9
Juzer Baatwala <57494882+dark733@users.noreply.github.com>
2023-12-12 17:29:11 +0530

    Add files via upload

--- fxml_manual/src/main/java/com/example/fxml_manual/PixelBlitz.java
+++ fxml_manual/src/main/java/com/example/fxml_manual/PixelBlitz.java
@@ -1,23 +1,55 @@
 package com.example.fxml_manual;
 
+import javafx.animation.Interpolator;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javax.sound.sampled.*;
+import javafx.scene.control.ProgressBar;
+import javafx.util.Duration;
+import javafx.animation.Timeline;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.scene.layout.StackPane;
 
 
 import java.io.File;
 import java.io.IOException;
+import java.util.EmptyStackException;
+import java.util.Objects;
 
 public class PixelBlitz extends Application {
+    private final loading_animation load_obj = new loading_animation();
+    private final sound_api obj_sound = new sound_api();
     @Override
     public void start(Stage stage) throws IOException, LineUnavailableException {
+        /* */
+        //load_obj.showLoadingScreen(stage); //loading screen dosent work
+        //load_obj.simulateLoading(stage);
+        /* */
+        load_obj.loading_fxml_file(stage);
         FXMLLoader fxmlLoader = new FXMLLoader(PixelBlitz.class.getResource("title_screen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 1000, Color.GRAY);
+        //link css
+        try{
+            //scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("styler.css")).toExternalForm());
+            String css;
+            css = Objects.requireNonNull(this.getClass().getResource("styler.css")).toExternalForm();
+            scene.getStylesheets().add(css);
+            System.out.println("Css file found!");
+
+        }
+        catch(NullPointerException e){
+            System.out.println(" Null pointer Exception Caught" + e);
+        }
+        catch (Exception e){
+            System.out.println("Exception Caught" + e);
+        }
         //sound api
-        sound_api obj_sound = new sound_api();
+
 
         // Get the controller instance
         titleScreen_ControllerClass controller = fxmlLoader.getController();
@@ -60,8 +92,88 @@ class sound_api{
             Clip clip = AudioSystem.getClip();
             clip.open(audioStream);
             clip.start();
-        } catch (UnsupportedAudioFileException e) {
+        }
+        catch (UnsupportedAudioFileException e) {
             throw new RuntimeException(e);
         }
+    }
+
+}
+class loading_animation {
+    private final ProgressBar progressBar = new ProgressBar(0);
+
+    public void showLoadingScreen(Stage stage) {
+        progressBar.setPrefWidth(500);
+        progressBar.setPrefHeight(100);
+
+        StackPane root = new StackPane(progressBar);
+        Scene loadingScene = new Scene(root, 400, 300);
+
+        stage.setScene(loadingScene);
+        stage.setTitle("Loading Animation Example");
+        stage.show();
+    }
+
+    public void simulateLoading(Stage stage) {
+        // Simulate loading progress with animation
+        Timeline timeline = new Timeline();
+        timeline.setCycleCount(1);
+        timeline.setAutoReverse(false);
+
+        // Animate progress bar value
+        KeyValue keyValue = new KeyValue(progressBar.progressProperty(), 1.0, Interpolator.LINEAR);
+        KeyFrame keyFrame = new KeyFrame(Duration.seconds(2), keyValue);
+        timeline.getKeyFrames().add(keyFrame);
+
+        // Show loading screen for 2 seconds and then switch to main scene
+        timeline.setOnFinished(event -> {
+            stage.setScene(null);
+            stage.hide();
+        });
+        timeline.play();
+    }
+    public void loading_fxml_file(Stage stage) throws IOException {
+        try{
+            System.out.println("Loading fxml file preview");
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("loadingScreen.fxml"));
+            Scene loadingScene = loader.load();
+            LoadingScreenController controller = loader.getController();
+
+
+            // Show loading screen
+            stage.setScene(loadingScene);
+            stage.setTitle("Loading...");
+            stage.show();
+            new Thread(() -> {
+                try {
+                    // Simulate loading
+                    Thread.sleep(2000);
+
+                    // Update progress bar on UI thread
+                    Platform.runLater(() -> controller.setProgress(1.0));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                catch (Exception e){
+                    System.out.println("Exception Caught" + e);
+                }
+            }).start();
+
+        }
+        catch(EmptyStackException e){
+            System.out.println("Stack is Empty Exception" + e);
+        }
+        catch(NullPointerException e){
+            System.out.println("NullPointerExceptionCaught " + e);
+        }
+        catch(Exception e){
+            System.out.println("Exception caught" + e);
+        }
+
+
+
+
+
+
     }
 }
--- fxml_manual/src/main/java/com/example/fxml_manual/titleScreen_ControllerClass.java
+++ fxml_manual/src/main/java/com/example/fxml_manual/titleScreen_ControllerClass.java
@@ -4,6 +4,7 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -12,6 +13,10 @@ import javafx.scene.layout.AnchorPane;
 
 
 public class titleScreen_ControllerClass {
+    menu_bar menu_bar_obj = new menu_bar();
+    game_one gameOne_obj = new game_one();
+    game_two gameTwo_obj = new game_two();
+    game_three gameThree_obj = new game_three();
 
     @FXML
     private AnchorPane TitleScreen;
@@ -47,34 +52,198 @@ public class titleScreen_ControllerClass {
     @FXML
 
     void switchBackToTitleScene(ActionEvent event) {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("title_screen.fxml"));
-            Parent titleScreenRoot = fxmlLoader.load();
-
-            if (stage != null) {
-                // Set the content of the stage's scene to the title screen root
-                stage.getScene().setRoot(titleScreenRoot);
-            } else {
-                System.out.println("Stage reference is null!");
-            }
-        } catch (IOException e) {
-            System.out.println("Error loading titleScreen.fxml: " + e.getMessage());
-        }
+        go_back(stage);
     }
 
 
-    public void game_one(ActionEvent event){
+    public void game_one_clicked(ActionEvent event){
         System.out.println("Game One!");
+        gameOne_obj.game_one_event();
         // Add logic for game one actions
     }
 
-    public void game_two(ActionEvent event){
+    public void game_two_clicked(ActionEvent event){
         System.out.println("Game Two!");
+        gameTwo_obj.game_two_event();
         // Add logic for game two actions
     }
 
-    public void game_three(ActionEvent event){
+    public void game_three_clicked(ActionEvent event){
         System.out.println("Game Three!");
+        gameThree_obj.game_three_event();
+
         // Add logic for game three actions
+    }
+    public void documentation_clicked(ActionEvent e){
+        System.out.println("documentation_clicked!");
+        menu_bar_obj.documentation_event(stage);
+
+    }
+    public void reference_clicked(ActionEvent e){
+        System.out.println("reference_clicked!");
+        menu_bar_obj.references_event(stage);
+
+    }
+    public void credits_clicked(ActionEvent e){
+        System.out.println("credits_clicked!");
+        menu_bar_obj.credits_event(stage);
+
+    }
+    public void license_clicked(ActionEvent e){
+        System.out.println("license_clicked!");
+        menu_bar_obj.license_event(stage);
+
+    }
+    public void faq_clicked(){
+        System.out.println("FAQ_clicked!");
+        menu_bar_obj.faq_event(stage);
+    }
+    public void forceexit_clicked(){
+        System.out.println("force_exit_clicked!");
+        menu_bar_obj.force_exit_event();
+    }
+    public void muteaudio_clicked(){
+        System.out.println("mute_audio_clicked!");
+        menu_bar_obj.mute_audio_event();
+    }
+    public void go_back(Stage stage){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("title_screen2.fxml"));
+            Parent gameSelectionRoot = fxmlLoader.load();
+
+            // Set the content of the stage's scene to the game selection root
+            //Node stage2;
+            stage.getScene().setRoot(gameSelectionRoot);
+
+        }
+        catch (IOException e) {
+            System.out.println("Something went wrong loading game_documentation.fxml: " + e.getMessage());
+            //e.printStackTrace();
+        }
+        catch(NullPointerException e){
+            System.out.println("Null Pointer Exception Caught " + e);
+        }
+        catch (Exception e){
+            System.out.println("Exception Caught" + e);
+        }
+
+    }
+}
+class menu_bar{
+    public void documentation_event(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("documentation.fxml"));
+            Parent gameSelectionRoot = fxmlLoader.load();
+
+            // Set the content of the stage's scene to the game selection root
+            //Node stage2;
+            stage.getScene().setRoot(gameSelectionRoot);
+
+        }
+        catch (IOException e) {
+            System.out.println("Something went wrong loading game_documentation.fxml: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        catch(NullPointerException e){
+            System.out.println("Null Pointer Exception Caught " + e);
+
+        }
+        catch(Exception e){
+            System.out.println("Exception Caught" + e);
+        }
+    }
+    public void references_event(Stage stage){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("References.fxml"));
+            Parent gameSelectionRoot = fxmlLoader.load();
+
+            // Set the content of the stage's scene to the game selection root
+            //Node stage2;
+            stage.getScene().setRoot(gameSelectionRoot);
+
+        }
+        catch (IOException e) {
+            System.out.println("Something went wrong loading reference.fxml: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        catch(NullPointerException e){
+            System.out.println("Null Pointer Exception Caught " + e);
+
+        }
+        catch(Exception e){
+            System.out.println("Exception Caught" + e);
+        }
+
+    }
+    public void credits_event(Stage stage){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Credits.fxml"));
+            Parent gameSelectionRoot = fxmlLoader.load();
+
+            // Set the content of the stage's scene to the game selection root
+            //Node stage2;
+            stage.getScene().setRoot(gameSelectionRoot);
+
+        }
+        catch (IOException e) {
+            System.out.println("Something went wrong loading game_documentation.fxml: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+    public void license_event(Stage stage){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("license.fxml"));
+            Parent gameSelectionRoot = fxmlLoader.load();
+
+            // Set the content of the stage's scene to the game selection root
+            //Node stage2;
+            stage.getScene().setRoot(gameSelectionRoot);
+
+        }
+        catch (IOException e) {
+            System.out.println("Something went wrong loading game_documentation.fxml: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+    public void faq_event(Stage stage){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("faq.fxml"));
+            Parent gameSelectionRoot = fxmlLoader.load();
+
+            // Set the content of the stage's scene to the game selection root
+            //Node stage2;
+            stage.getScene().setRoot(gameSelectionRoot);
+
+        }
+        catch (IOException e) {
+            System.out.println("Something went wrong loading game_documentation.fxml: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+    public void force_exit_event(){
+
+    }
+    public void mute_audio_event(){
+
+    }
+}
+class game_one{
+    public void game_one_event(){
+
+    }
+}
+class game_two{
+    public void game_two_event(){
+
+    }
+}
+class game_three{
+    public void game_three_event(){
+
     }
 }
