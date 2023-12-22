bef200b339a75afde0756276c0eaa59cb7dfe281
Juzer Baatwala <57494882+dark733@users.noreply.github.com>
2023-12-22 19:06:53 +0530

    Add files via upload

---
+++ pixelBlitzProject/src/main/java/com/example/fxml_manual/game_one.java
@@ -0,0 +1,16 @@
+package com.example.fxml_manual;
+
+
+import javax.swing.*;
+abstract class game_one_i{
+    abstract void game_one_event();
+}
+
+class game_one extends game_one_i{
+    public void game_one_event() {
+        //JFrame frame = new JFrame();
+        System.out.println("Initializing Game one");
+        //Game One Implemented here
+
+    }
+}
---
+++ pixelBlitzProject/src/main/java/com/example/fxml_manual/game_two.java
@@ -0,0 +1,12 @@
+package com.example.fxml_manual;
+abstract class game_two_i{
+    abstract void game_two_event();
+}
+
+class game_two extends game_two_i{
+    public void game_two_event() {
+        System.out.println("Initialising game Two");
+        //Game Two Implemented here
+
+    }
+}
